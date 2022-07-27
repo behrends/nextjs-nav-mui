d273c69107317f8c3f0f566da84a7b32a3001739
Erik Behrends <behrends@users.noreply.github.com>
2022-07-27 14:18:52 +0200

    Replace layout with MUI-components

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
 import Head from 'next/head';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import styles from '../styles/Home.module.css';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Beispiel mit MUI</title>
         <meta name="description" content="MUI Example" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Willkommen!</h1>
+      <Box
+        minHeight="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant="h1">Willkommen!</Typography>
 
         <Button variant="contained">Hello MUI!</Button>
-      </main>
-
-      <footer className={styles.footer}>TODO copyright</footer>
-    </div>
+      </Box>
+    </>
   );
 }
