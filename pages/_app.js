e035d52a93260b1ab51bf220ccfd85a6c630b9a5
Erik Behrends <behrends@users.noreply.github.com>
2022-07-27 14:34:17 +0200

    Remove _document

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,11 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="initial-scale=1, width=device-width"
         />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
       </Head>
       <CssBaseline />
       <Component {...pageProps} />
