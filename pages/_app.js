42d34602e2742edb32301d4a89fb7cb82947a837
Erik Behrends <behrends@users.noreply.github.com>
2022-07-28 20:39:35 +0200

    Layout not layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import CssBaseline from '@mui/material/CssBaseline';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
