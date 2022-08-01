9ad3eff6b47f558a8ac484f656a7b551ef4518f5
Erik Behrends <behrends@users.noreply.github.com>
2022-08-01 16:38:22 +0200

    Load stylesheet in _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,10 +11,6 @@ function MyApp({ Component, pageProps }) {
           content="initial-scale=1, width=device-width"
         />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
       </Head>
       <CssBaseline />
       <Layout
