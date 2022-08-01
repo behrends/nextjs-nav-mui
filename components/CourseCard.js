479389e23e5629cbf1c22dc5d5f3b869db197f8f
Erik Behrends <behrends@users.noreply.github.com>
2022-08-01 14:26:45 +0200

    State in CourseCard

--- components/CourseCard.js
+++ components/CourseCard.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
 export default function CourseCard({ name }) {
+  // [stateVariable, updateFunction] = useState(initialState)
+  const [isSelected, setSelected] = useState(false);
   return (
-    <Card sx={{ minWidth: 275, my: 2 }} raised>
+    <Card
+      sx={{
+        minWidth: 275,
+        my: 2,
+        cursor: 'pointer',
+        background: isSelected ? 'yellow' : 'white',
+      }}
+      raised={isSelected}
+      onClick={() => setSelected(!isSelected)}
+    >
       <CardContent>
         <Typography variant="h4">{name}</Typography>
       </CardContent>
