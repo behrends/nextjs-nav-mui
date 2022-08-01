0467cc2436f42bc11947c0ebe0a8c2922256f55b
Erik Behrends <behrends@users.noreply.github.com>
2022-08-01 16:12:57 +0200

    Fetch courses from db.json

--- components/Courses.js
+++ components/Courses.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react';
+import Typography from '@mui/material/Typography';
 import CourseCard from './CourseCard';
 
 export default function Courses() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    // fetch data from github.com/behrends/nextjs-nav-mui/db.json
+    // (fake REST server, see https://my-json-server.typicode.com)
+    const loadData = async () => {
+      const response = await fetch(
+        'https://my-json-server.typicode.com/behrends/nextjs-nav-mui/courses'
+      );
+      const data = await response.json();
+      setCourses(data);
+    };
+    loadData().catch(console.error);
+  }, []);
+
+  if (courses.length === 0)
+    return <Typography variant="h5">Keine Kurse</Typography>;
+
   return (
     <>
-      <CourseCard key="1" name="TIF21A" />
-      <CourseCard key="2" name="TIF21B" />
-      <CourseCard key="3" name="TIF22A" />
+      {courses.map((course) => (
+        <CourseCard key={course.id} name={course.name} />
+      ))}
     </>
   );
 }
