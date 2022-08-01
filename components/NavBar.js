8dc4d0bee1ed79274b5324e80856864a7810d1c5
Erik Behrends <behrends@users.noreply.github.com>
2022-08-01 15:25:17 +0200

    Decorate active screen in navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,19 +1,28 @@
 import * as React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { AppBar, Box, Toolbar, Button } from '@mui/material';
 
 export default function NavBar({ pages }) {
+  const router = useRouter();
   return (
     <AppBar position="fixed">
       <Toolbar>
         <Box flex={1} display="flex">
-          {pages.map((page) => (
-            <Link key={page.name} href={page.route} passHref>
-              <Button component="a" color="inherit">
-                {page.name}
-              </Button>
-            </Link>
-          ))}
+          {pages.map((page) => {
+            // underline current screen in navbar if equal to route
+            const style = {
+              textDecoration:
+                router.asPath === page.route ? 'underline' : 'none',
+            };
+            return (
+              <Link key={page.name} href={page.route} passHref>
+                <Button component="a" color="inherit" sx={style}>
+                  {page.name}
+                </Button>
+              </Link>
+            );
+          })}
         </Box>
         <Link href="/login" passHref>
           <Button component="a" color="inherit">
